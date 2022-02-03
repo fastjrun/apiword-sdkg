a9b72a5e9f14693d89a6badf38577842f0b986b7
fastjrun <fastjrun@139.com>
2022-02-03 19:57:07 +0800

    更新为1.4

--- deploy-nexus-gitee.groovy
+++ deploy-nexus-gitee.groovy
@@ -1,6 +1,6 @@
 node {
     stage('git chekout') {
-        git branch: "master", url: 'https://gitee.com/fastjrun/sdkg.git'
+        git branch: "master", url: 'https://gitee.com/fastjrun/apiworld-sdkg.git'
     }
     stage('mvn deploy') {
         sh 'sh build.sh publish_apiworld_sdkg'
