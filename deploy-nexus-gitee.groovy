9c57dae4aaf43d89158d8f28136dbc22a225890a
fastjrun <fastjrun@139.com>
2022-02-04 10:49:42 +0800

    修改流水线

--- deploy-nexus-gitee.groovy
+++ deploy-nexus-gitee.groovy
@@ -1,8 +1,9 @@
 node {
     stage('git chekout') {
+        // 镜像仓库至gitee
         git branch: "master", url: 'https://gitee.com/fastjrun/apiworld-sdkg.git'
     }
     stage('mvn deploy') {
-        sh 'sh build.sh publish_apiworld_sdkg'
+        sh 'mvn clean deploy -Prelease -pl apiworld-sdkg-client,apiworld-sdkg-provider,apiworld-sdkg-generator -am'
     }
 }
